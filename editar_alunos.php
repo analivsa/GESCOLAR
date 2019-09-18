5431030baacc7f78fb29d022df416eecf067b201
Aluno <aluno@etec.sp.gov.br>
2019-09-18 14:42:30 -0300

    atualizar

--- editar_alunos.php
+++ editar_alunos.php
@@ -2,16 +2,16 @@
 /**
  * Arquivo para registrar os dados de um aluno no banco de dados.
  */
-if(isset($_REQUEST['atualizar']))
+try
 {
-    try
+    if(isset($_REQUEST['atualizar']))
     {
-        include 'includes/conexao.php';
 
-        $sql = "UPDATE alunos SET nome = ?, data nascimento = ?, sexo =?,
+        include 'includes/conexao.php';
+        $sql = "UPDATE alunos SET nome = ?, data_nascimento = ?, sexo =?,
                                   genero = ?, cpf =?, cidade = ?, estado = ?,
                                   bairro = ?, rua = ?, cep = ?
-                              WHERE id_aluno = ?) ";
+                WHERE id = ? ";
         
         $stmt = $conexao->prepare($sql);
         $stmt->bindParam(1, $_REQUEST['nome']);
@@ -27,27 +27,48 @@ if(isset($_REQUEST['atualizar']))
         $stmt->bindParam(12, $_REQUEST['id_aluno']);
         $stmt->execute();
     
-    } catch(Exception $e) {
-        echo $e->getMessage(); 
-    }
-}
+    } 
+    
+    if(isset($_REQUEST['excluir']))
+    {
+        $stml = $conexao->prepare("DELETE FROM aluno WHERE id = ?");
+        $stml->bindParam(1, $_REQUEST['id_aluno']);
+        $stml->execute();
+        header("location: lista_alunos.php");
+    }    
 
+    $stml = $conexao->prepare("SELECT * FROM aluno WHERE id = ?");
+    $stml->bindParam(1, $_REQUEST['id_aluno']);
+    $stml->execute();
+    $aluno = $stml->fechObject();
+
+} catch(Exception $e) {
+    echo $e->getMessage(); 
+}
 ?>
 <link href="css/estilos.css" type="text/css" rel="stylesheet" />
-
 <?php include_once 'includes/cabecalho.php' ?>
-
 <div>
+
 <fieldset>
     <legend>Cadastro de Aluno </legend>
         <form action="editar_alunos.php?atualizar=true">
             <label>Nome: <input type="text" name="nome" required /> </label>
+          
             <label>Cidade: <input type="text" name="cidade" required /> </label>
+          
             <label>CEP: <input type="text" name="cep" required /> </label>
+          
             <label>Bairro: <input type="text" name="bairro" required /> </label>
+          
             <label>Rua: <input type="text" name="rua" required /> </label>
+          
             <label>Estado: <input type="text" name="estado" required /> </label>
+          
             <label>Data Nasc: <input type="text" name="data_nascimento" required /> </label>
+          
+            <a href="editar_alunos.php?excluir=true&id=<?= $aluno->id ?>">Excluir</a>
+            
             <button type="submit">Salvar</button>
         </form>
     </legend>
